bbdb1dc6ccc0cdf6cbc5fb4c6590b2c626a555f8
viktor.k <viktor.kharts@gmail.com>
2024-04-05 21:42:54 +0200

    initial commit

---
+++ src/hotels/hotels.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+
+@Controller('hotels')
+export class HotelsController {
+  @Get()
+  getHotels() {
+
+  }
+  
+  @Get()
+  getAllHotels() {
+
+  }
+
+  @Post()
+  createHotel() {
+
+  }
+
+  @Patch()
+  updateHotel() {
+
+  }
+
+  @Delete()
+  deleteHotel() {
+
+  }
+}
